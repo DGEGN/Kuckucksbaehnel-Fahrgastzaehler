c2f93095c2908b7b9a51088cfe8226654da001f4
DJPassi1 <Pascal_leon_schneider@web.de>
2026-08-16 09:18:49 +0200

    Add new wagons to the wagon list

--- app.js
+++ app.js
@@ -102,7 +102,10 @@ const WAGEN = [
   { id: "wagen4", name: "Wagen 4", sitzplaetze: 70, bild: "assets/wagen/2455.jpg" },
   { id: "wagen5", name: "Wagen 5", sitzplaetze: 88, bild: "assets/wagen/4918.jpg" },
   { id: "wagen6", name: "Wagen 6", sitzplaetze: 53, bild: "assets/wagen/82813.jpg" },
-  { id: "wagen7", name: "Wagen 7", sitzplaetze: 56, bild: "assets/wagen/85034.jpg" }
+  { id: "wagen7", name: "Wagen 7", sitzplaetze: 56, bild: "assets/wagen/85034.jpg" }, 
+  { id: "wagen8", name: "Wagen 8", sitzplaetze: 44, bild: "assets/wagen/85466.jpg" },
+  { id: "wagen9", name: "Wagen 9", sitzplaetze: 44, bild: "assets/wagen/84803.jpg" },
+  { id: "wagen10", name: "Wagen 10", sitzplaetze: 44, bild: "assets/wagen/301764.jpg" },
 ];
 let selectedWagen = new Set();
 
